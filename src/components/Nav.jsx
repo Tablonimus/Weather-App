279bafc051617ce1695b42385d81345deb139ff1
tablonimus <lautamartin1995@gmail.com>
2022-09-23 15:46:24 -0300

    clear

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../logoHenry.png";
+// import Logo from "../logoHenry.png";
 import SearchBar from "./SearchBar.jsx";
 import "./Nav.css";
 
@@ -7,15 +7,15 @@ function Nav({ onSearch }) {
   return (
     <nav className="flexing">
       <span>
-        <img
+        {/* <img
           id="logoHenry"
           src={Logo}
           alt=""
           width="35"
           height="40"
           className="d-inline-block align-text-top"
-        />
-        <span>Henry - Weather App</span>
+        /> */}
+        <h1>World Wide Weather</h1>
       </span>
 
       <SearchBar onSearch={onSearch} />
