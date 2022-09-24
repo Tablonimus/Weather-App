455a60f8b01de97251ed858a6e017df623ebf603
tablonimus <lautamartin1995@gmail.com>
2022-09-23 23:46:25 -0300

    bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react'; //Se agregó el { useState }
-import './App.css';
-import Cards from './components/Cards.jsx'
-import Nav from './components/Nav.jsx';
+import React, { useState } from "react"; //Se agregó el { useState }
+import "./App.css";
+import Cards from "./components/Cards.jsx";
+import Nav from "./components/Nav.jsx";
 
 export default function App() {
-
-  const apiKey = '4ae2636d8dfbdc3044bede63951a019b';
+  const apiKey = "4ae2636d8dfbdc3044bede63951a019b";
 
   const [cities, setCities] = useState([]);
 
   function onSearch(ciudad) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
-      .then(r => r.json())
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`
+    )
+      .then((r) => r.json())
       .then((recurso) => {
         if (recurso.main !== undefined) {
           const ciudad = {
@@ -22,21 +23,22 @@ export default function App() {
             wind: recurso.wind.speed,
             temp: recurso.main.temp,
             name: recurso.name,
+            country: recurso.sys.country,
             weather: recurso.weather[0].main,
             clouds: recurso.clouds.all,
             latitud: recurso.coord.lat,
-            longitud: recurso.coord.lon
+            longitud: recurso.coord.lon,
           };
-          setCities(oldCities => [...oldCities, ciudad]);
+          setCities((oldCities) => [...oldCities, ciudad]);
         } else {
           alert("Ciudad no encontrada");
         }
+        console.log(recurso);
       });
-
   }
 
   function onClose(id) {
-    setCities(oldCities => oldCities.filter(c => c.id !== id));
+    setCities((oldCities) => oldCities.filter((c) => c.id !== id));
   }
 
   return (
@@ -46,5 +48,3 @@ export default function App() {
     </div>
   );
 }
-
-
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,14 +7,7 @@ function Nav({ onSearch }) {
   return (
     <nav className="flexing">
       <span>
-        {/* <img
-          id="logoHenry"
-          src={Logo}
-          alt=""
-          width="35"
-          height="40"
-          className="d-inline-block align-text-top"
-        /> */}
+    
         <h1>World Wide Weather</h1>
       </span>
 
